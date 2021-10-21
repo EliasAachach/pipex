8b9e37b1623c026a6ee545b2d1095a393a5ea1c6
EliasAachach <elaachac@student.42.fr>
2021-10-22 01:33:11 +0200

    filename check : ok

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,15 +6,17 @@
 /*   By: elaachac <elaachac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/21 14:49:22 by elaachac          #+#    #+#             */
-/*   Updated: 2021/10/21 19:50:57 by elaachac         ###   ########.fr       */
+/*   Updated: 2021/10/22 00:28:06 by elaachac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 
-#include <stdio.h>
-#include <stdlib.h>
+# include <stdio.h>
+# include <stdlib.h>
 # include <unistd.h>
+
+void	check_file(char *pathname);
 
 #endif
--- srcs/checkers.c
+++ srcs/checkers.c
@@ -1,25 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex.c                                            :+:      :+:    :+:   */
+/*   checkers.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: elaachac <elaachac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/21 14:51:10 by elaachac          #+#    #+#             */
-/*   Updated: 2021/10/22 00:28:47 by elaachac         ###   ########.fr       */
+/*   Created: 2021/10/22 00:18:29 by elaachac          #+#    #+#             */
+/*   Updated: 2021/10/22 01:09:15 by elaachac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pipex.h"
 
-void    check_args(char **argv)
+void	check_file(char *pathname)
 {
-	check_file(argv[1]);
-	check_file(argv[4]);
-}
-
-int main(int argc, char **argv)
-{
-	check_args(argv);
-	return (0);
+	if (access(pathname, F_OK) == -1)
+	{
+		perror(pathname);
+		exit(0);
+	}
 }
